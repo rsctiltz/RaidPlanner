840eed46de5b71e1ca290fc9878286636e359b7e
rsctiltz <rsctiltz@gmail.com>
2014-06-04 15:35:46 -0500

    Raid Planner Hackwich

---
+++ RaidPlanner/RaidPlanner/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RaidPlanner
+//
+//  Created by Ryan Tiltz on 6/4/14.
+//  Copyright (c) 2014 Ryan Tiltz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ RaidPlanner/RaidPlanner/DetailViewController.m
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.m
+//  RaidPlanner
+//
+//  Created by Ryan Tiltz on 6/4/14.
+//  Copyright (c) 2014 Ryan Tiltz. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+@end
